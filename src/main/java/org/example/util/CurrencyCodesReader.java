0ecbac25364b7d91087e3b182e8d3c3f18670421
Dawid Żeleźniak <zelekdidi@tlen.pl>
2024-12-05 08:47:45 +0100

    new class to read currency codes

---
+++ src/main/java/org/example/util/CurrencyCodesReader.java
@@ -0,0 +1,34 @@
+package org.example.util;
+
+import jakarta.annotation.PostConstruct;
+import lombok.extern.slf4j.Slf4j;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.HashSet;
+import java.util.Set;
+
+@Slf4j
+public class CurrencyCodesReader {
+
+    private static final String FILE_NAME = "codes.txt";
+
+    @PostConstruct
+    public static Set<String> readCodes() {
+        Set<String> codes = new HashSet<>();
+
+        try (var br = new BufferedReader(new FileReader(FILE_NAME))) {
+            String code;
+            int codesCounter = 0;
+            while ((code = br.readLine()) != null) {
+                codesCounter++;
+                codes.add(code);
+            }
+            log.info("Codes loaded : {}", codesCounter);
+        } catch (IOException e) {
+            throw new RuntimeException("File " + FILE_NAME + " not found.");
+        }
+        return codes;
+    }
+}
